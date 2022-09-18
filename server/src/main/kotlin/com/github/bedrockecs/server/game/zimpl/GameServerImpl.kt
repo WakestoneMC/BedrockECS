81cc12d5d036a094ec03c690485f04ac3aeac92d
johnbanq <johnbanq@gmail.com>
2022-09-18 16:01:43 +0100

    feat(game): initial game logic framework

---
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/zimpl/GameServerImpl.kt
@@ -0,0 +1,68 @@
+package com.github.bedrockecs.server.game.zimpl
+
+import com.github.bedrockecs.server.GlobalExceptionHook
+import com.github.bedrockecs.server.game.GameServer
+import com.github.bedrockecs.server.game.ext.GameConfiguration
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.AnnotationConfigApplicationContext
+
+class GameServerImpl(
+    dispatcher: CoroutineDispatcher,
+    private val exceptionHook: GlobalExceptionHook,
+    private val coreContext: ApplicationContext
+) : GameServer {
+    companion object {
+        val INTRINSIC_GAME_CONFIGS: List<Class<*>> = listOf()
+    }
+
+    private val scope = CoroutineScope(dispatcher)
+
+    private var job: Job? = null
+
+    private suspend fun main() {
+        log.debug("starting game server context")
+        val context = createContext()
+        context.refresh()
+        context.start()
+        try {
+            log.debug("game server started")
+            awaitCancellation()
+        } finally {
+            log.debug("game server shutting down")
+            context.stop()
+            log.debug("game server stopped")
+        }
+    }
+
+    private fun createContext(): AnnotationConfigApplicationContext {
+        val context = AnnotationConfigApplicationContext()
+        context.parent = coreContext
+        INTRINSIC_GAME_CONFIGS.forEach { context.register(it) }
+        coreContext.getBeansWithAnnotation(GameConfiguration::class.java)
+            .forEach { context.register(it.value::class.java) }
+        return context
+    }
+
+    fun start() {
+        val job = scope.launch { main() }
+        job.invokeOnCompletion {
+            if (it != null) {
+                exceptionHook.report(it)
+            }
+        }
+        this.job = job
+    }
+
+    fun stop() {
+        runBlocking {
+            job?.cancelAndJoin()
+        }
+    }
+}
---
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/zimpl/Log.kt
@@ -0,0 +1,5 @@
+package com.github.bedrockecs.server.game.zimpl
+
+import org.slf4j.LoggerFactory
+
+val log = LoggerFactory.getLogger("game")
