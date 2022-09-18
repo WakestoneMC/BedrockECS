87e64531b7d8bad2fd396516eb4af49a2f9c5861
johnbanq <johnbanq@gmail.com>
2022-09-18 16:39:52 +0100

    refactor(game): rename parameters for block registry

--- server/src/main/kotlin/com/github/bedrockecs/server/game/registry/MutableBlockRegistry.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/registry/MutableBlockRegistry.kt
@@ -13,12 +13,12 @@ interface MutableBlockRegistry : BlockRegistry {
     /**
      * set a runtime id for this block of this type
      */
-    fun setRuntimeIDFor(typeState: BlockTypeComponent, rid: Int)
+    fun setRuntimeIDFor(type: BlockTypeComponent, rid: Int)
 
     /**
      * try to allocate a runtime id for this block of this type
      */
-    fun allocateRuntimeIDFor(typeState: BlockTypeComponent): Int
+    fun allocateRuntimeIDFor(type: BlockTypeComponent): Int
 
     // Default BlockComponents //
 
@@ -36,7 +36,7 @@ interface MutableBlockRegistry : BlockRegistry {
      * add an override at type level, null for removing that component
      */
     fun addTypeDefaultOverride(
-        typeState: BlockTypeComponent,
+        type: BlockTypeComponent,
         clazz: Class<out BlockComponent>,
         component: BlockComponent?
     )
@@ -45,7 +45,7 @@ interface MutableBlockRegistry : BlockRegistry {
      * remove a default component at type level
      */
     fun removeTypeDefaultOverride(
-        typeState: BlockTypeComponent,
+        type: BlockTypeComponent,
         clazz: Class<out BlockComponent>
     )
 }
