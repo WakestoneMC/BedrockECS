96fc415b5b82582e815f3e357e9a3737bba357f2
johnbanq <johnbanq@gmail.com>
2022-09-19 15:05:07 +0100

    feat(game): revise entity position to consider rotation

---
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/data/FloatRotation.kt
@@ -0,0 +1,9 @@
+package com.github.bedrockecs.server.game.data
+
+/**
+ * represents rotation
+ */
+data class FloatRotation(
+    val x: Float,
+    val z: Float
+)
--- server/src/main/kotlin/com/github/bedrockecs/server/game/db/entity/data/EntityPositionComponent.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/db/entity/data/EntityPositionComponent.kt
@@ -1,13 +1,15 @@
 package com.github.bedrockecs.server.game.db.entity.data
 
 import com.github.bedrockecs.server.game.data.FloatBlockPosition
+import com.github.bedrockecs.server.game.data.FloatRotation
 
 /**
  * represents an entity with position
  * @apiNote this also means it takes part in chunk loading and gets loaded / unloaded as well!
  */
 data class EntityPositionComponent(
-    var pos: FloatBlockPosition
+    var pos: FloatBlockPosition,
+    var rot: FloatRotation
 ) : EntityComponent {
     override val type: String
         get() = "becs:position"
