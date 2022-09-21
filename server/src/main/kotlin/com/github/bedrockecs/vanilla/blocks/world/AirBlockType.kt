ba73d783272af1a2148a0acd759ecd1dbc04474b
johnbanq <johnbanq@gmail.com>
2022-09-21 09:25:06 +0100

    feat(vanilla): refine generated code design

--- server/src/main/kotlin/com/github/bedrockecs/vanilla/blocks/world/AirBlockType.kt
+++ server/src/main/kotlin/com/github/bedrockecs/vanilla/blocks/world/AirBlockType.kt
@@ -1,14 +1,24 @@
 package com.github.bedrockecs.vanilla.blocks.world
 
-class AirBlockType private constructor() : VanillaBlockType {
+class AirBlockType private constructor(
+    override val runtimeID: Short
+) : VanillaBlockType {
     companion object {
-        val instance134 = AirBlockType()
+        private val instance134 = AirBlockType(134)
+        val primary = instance134
         val allInstances: List<AirBlockType> = listOf(instance134)
+    }
+
+    fun with(): AirBlockType {
+        val e = AirBlockType(0)
+        return allInstances.find { it.compareVariantProperties(e) }!!
+    }
+
+    private fun compareVariantProperties(other: AirBlockType): Boolean {
+        var ret = true
+        return ret
     }
 
     override val blockType: String
         get() = "minecraft:air"
-
-    override val runtimeID: Short
-        get() = 134
 }
--- server/src/main/kotlin/com/github/bedrockecs/vanilla/blocks/world/DirtBlockType.kt
+++ server/src/main/kotlin/com/github/bedrockecs/vanilla/blocks/world/DirtBlockType.kt
@@ -1,14 +1,27 @@
 package com.github.bedrockecs.vanilla.blocks.world
 
-class DirtBlockType private constructor() : VanillaBlockType {
+data class DirtBlockType private constructor(
+    override val runtimeID: Short,
+    val dirtType: String
+) : VanillaBlockType {
     companion object {
-        val instance4484 = DirtBlockType()
-        val allInstances: List<DirtBlockType> = listOf(instance4484)
+        private val instance4484 = DirtBlockType(4484, "normal")
+        private val instance4485 = DirtBlockType(4485, "coarse")
+        val primary = instance4484
+        val allInstances: List<DirtBlockType> = listOf(instance4484, instance4485)
+    }
+
+    fun with(dirtType: String = this.dirtType): DirtBlockType {
+        val e = DirtBlockType(0, dirtType)
+        return allInstances.find { it.compareVariantProperties(e) }!!
+    }
+
+    private fun compareVariantProperties(other: DirtBlockType): Boolean {
+        var ret = true
+        ret = ret && this.dirtType == other.dirtType
+        return ret
     }
 
     override val blockType: String
-        get() = "minecraft:air"
-
-    override val runtimeID: Short
-        get() = 4484
+        get() = "minecraft:dirt"
 }
