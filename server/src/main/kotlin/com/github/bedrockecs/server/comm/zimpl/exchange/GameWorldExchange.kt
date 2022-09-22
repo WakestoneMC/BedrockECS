32e438db667651add44ebf0a3432f0fe8938fb0b
johnbanq <johnbanq@gmail.com>
2022-09-22 17:58:14 +0100

    fix(comm): write aoi in unit of chunk, not blocks

--- server/src/main/kotlin/com/github/bedrockecs/server/comm/zimpl/exchange/GameWorldExchange.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/comm/zimpl/exchange/GameWorldExchange.kt
@@ -66,7 +66,7 @@ class GameWorldExchange {
         if (session != null) {
             session.aoiBlockRadius = min(packet.radius * SUBCHUNK_SIZE, 16 * SUBCHUNK_SIZE)
             val resp = ChunkRadiusUpdatedPacket()
-            resp.radius = session.aoiBlockRadius
+            resp.radius = session.aoiBlockRadius / SUBCHUNK_SIZE
             connection.sendPacket(resp, NetworkConnection.Latency.IMMEDIATELY)
         }
     }
