a1493008b08a55bdd045cc1c4a431a10a679641b
johnbanq <johnbanq@gmail.com>
2022-09-22 15:38:38 +0100

    refactor(game): revise chunk loading system

--- server/src/main/kotlin/com/github/bedrockecs/server/game/chunkloading/zimpl/system/ChunkLoadingSystem.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/chunkloading/zimpl/system/ChunkLoadingSystem.kt
@@ -3,30 +3,51 @@ package com.github.bedrockecs.server.game.chunkloading.zimpl.system
 import com.github.bedrockecs.server.game.chunkloading.entity.EntityChunkLoadingComponent
 import com.github.bedrockecs.server.game.data.ChunkPosition
 import com.github.bedrockecs.server.game.db.GameDB
-import com.github.bedrockecs.server.game.db.entity.EntityScanConfig
 import com.github.bedrockecs.server.game.db.entity.data.EntityPositionComponent
+import com.github.bedrockecs.server.game.db.entity.scan
+import com.github.bedrockecs.server.game.system.CommonTickOrders
 import com.github.bedrockecs.server.game.system.System
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import java.util.Collections.synchronizedSet
 
 @Component
 class ChunkLoadingSystem(
     private val db: GameDB
 ) : System {
+    companion object {
+        val log = LoggerFactory.getLogger("game.chunk-loading")
+    }
+
     override val tickOrder: Int
-        get() = Int.MIN_VALUE + 10_000
+        get() = CommonTickOrders.CHUNK_LOADING
 
     override fun tick() {
-        val shouldLoadChunks = synchronizedSet(HashSet<ChunkPosition>())
-        db.entities.scan(
-            EntityScanConfig(),
-            arrayOf(EntityChunkLoadingComponent::class.java, EntityPositionComponent::class.java)
-        ) { _, arr ->
-            val loadingComponent = arr[0] as EntityChunkLoadingComponent
-            val positionComponent = arr[1] as EntityPositionComponent
+        val shouldLoadChunks = computeShouldLoadChunks()
 
-            val selfChunk = positionComponent.pos.toChunk()
-            val radius = loadingComponent.radius
+        val loadedChunks = db.listLoadedChunks()
+        val toLoadChunks = shouldLoadChunks - loadedChunks
+        val toUnLoadChunks = loadedChunks - shouldLoadChunks
+
+        try {
+            toLoadChunks.map {
+                log.debug("loading chunk [chunk={}]", it)
+                db.loadChunk(it)
+            }.forEach { it.join() }
+            toUnLoadChunks.map {
+                log.debug("unloading chunk [chunk={}]", it)
+                db.unloadChunk(it)
+            }.forEach { it.join() }
+        } catch (ex: Throwable) {
+            log.error("error performing chunk loading [toLoad={}, toUnLoad={}]", toLoadChunks, toUnLoadChunks)
+            throw ex
+        }
+    }
+
+    private fun computeShouldLoadChunks(): HashSet<ChunkPosition> {
+        val shouldLoadChunks = HashSet<ChunkPosition>()
+        db.entities.scan<EntityChunkLoadingComponent, EntityPositionComponent> { eid, loading, pos ->
+            val selfChunk = pos.pos.toChunk()
+            val radius = loading.radius
             for (deltaX in (-radius..radius)) {
                 for (deltaZ in (-radius..radius)) {
                     val chunk = ChunkPosition(selfChunk.x + deltaX, selfChunk.z + deltaZ, selfChunk.dim)
@@ -34,9 +55,6 @@ class ChunkLoadingSystem(
                 }
             }
         }
-        val loadedChunks = db.listLoadedChunks()
-        shouldLoadChunks.removeAll(loadedChunks.toSet())
-
-        shouldLoadChunks.map { db.loadChunk(it) }.forEach { it.join() }
+        return shouldLoadChunks
     }
 }
