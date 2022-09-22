0a1adc912a0525f3fe2b94ef1376662361ff7d0f
johnbanq <johnbanq@gmail.com>
2022-09-22 15:42:49 +0100

    feat(comm): get disconnect player to work

--- server/src/main/kotlin/com/github/bedrockecs/server/comm/zimpl/exchange/ActionUpdateExchange.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/comm/zimpl/exchange/ActionUpdateExchange.kt
@@ -1,13 +1,17 @@
 package com.github.bedrockecs.server.comm.zimpl.exchange
 
 import com.github.bedrockecs.server.comm.game.action.Action
+import com.github.bedrockecs.server.comm.game.update.DisconnectPlayerUpdate
 import com.github.bedrockecs.server.comm.game.update.Update
+import com.github.bedrockecs.server.comm.zimpl.log
 import org.springframework.stereotype.Component
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
 @Component
-class ActionUpdateExchange {
+class ActionUpdateExchange(
+    private val connections: PlayerConnectionExchange
+) {
 
     private val actionsLock = ReentrantLock()
 
@@ -19,15 +23,22 @@ class ActionUpdateExchange {
         }
     }
 
-    fun sendUpdates(actions: List<Update>) {
-        // no-op for now //
-    }
-
     fun collectActions(): List<Action> {
         actionsLock.withLock {
             val oldActions = actions
             actions = mutableListOf()
             return oldActions
+        }
+    }
+
+    fun sendUpdates(actions: List<Update>) {
+        actions.forEach { sendUpdate(it) }
+    }
+
+    fun sendUpdate(update: Update) {
+        when (update) {
+            is DisconnectPlayerUpdate -> connections.connectionForUUID(update.uuid)?.disconnect()
+            else -> log.warn("ignoring unknown update [update={}]", update)
         }
     }
 }
