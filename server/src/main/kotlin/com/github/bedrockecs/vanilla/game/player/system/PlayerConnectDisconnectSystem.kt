3e39f40eda8be36ec59f2b9ad41047541daa0174
johnbanq <johnbanq@gmail.com>
2022-09-23 14:24:38 +0100

    refactor(vanilla): reorg the conn/disconn system

--- server/src/main/kotlin/com/github/bedrockecs/vanilla/game/player/system/PlayerConnectDisconnectSystem.kt
+++ server/src/main/kotlin/com/github/bedrockecs/vanilla/game/player/system/PlayerConnectDisconnectSystem.kt
@@ -7,6 +7,7 @@ import com.github.bedrockecs.server.game.chunkloading.entity.EntityChunkLoadingC
 import com.github.bedrockecs.server.game.data.FloatBlockPosition
 import com.github.bedrockecs.server.game.db.GameDB
 import com.github.bedrockecs.server.game.db.entity.data.EntityPositionComponent
+import com.github.bedrockecs.server.game.system.CommonTickOrders
 import com.github.bedrockecs.server.game.system.System
 import com.github.bedrockecs.vanilla.game.player.entity.PlayerEntityType
 import com.github.bedrockecs.vanilla.game.player.entity.PlayerIdentifierComponent
@@ -24,32 +25,38 @@ class PlayerConnectDisconnectSystem(
     private val UNIVERSAL_ROT = Vector3f.from(1.0f, 0.0f, 0.0f)
 
     override val tickOrder: Int
-        get() = Int.MIN_VALUE + 5_000
+        get() = CommonTickOrders.PLAYER_INTERACTION
 
     override fun tick() {
         mailbox.listActions()
             .filterIsInstance<PlayerConnectedAction>()
-            .forEach {
-                val eid = db.entities.create(
-                    PlayerEntityType.TYPE,
-                    mutableSetOf(
-                        PlayerIdentifierComponent(
-                            displayName = it.identifiers.displayName!!,
-                            uuid = it.identifiers.playerUUID!!
-                        ),
-                        EntityPositionComponent(UNIVERSAL_SPAWN, UNIVERSAL_ROT), // TODO: get proper spawn logic
-                        EntityChunkLoadingComponent(radius = 4) // TODO: move this to chunk loading?
-                    )
-                )
-                mapContext.onPlayerConnected(it.identifiers, eid)
-            }
+            .forEach(this::handleConnectedAction)
+
         mailbox.listActions()
             .filterIsInstance<PlayerDisconnectedAction>()
-            .mapNotNull {
-                val eid = mapContext.findPlayerByUUID(it.identifiers.playerUUID!!)
-                mapContext.onPlayerDisconnected(it.identifiers)
-                eid
-            }
-            .forEach { db.entities.destroy(it) }
+            .forEach(this::handleDisconnectedAction)
+    }
+
+    private fun handleConnectedAction(it: PlayerConnectedAction) {
+        val eid = db.entities.create(
+            PlayerEntityType.TYPE,
+            mutableSetOf(
+                PlayerIdentifierComponent(
+                    displayName = it.identifiers.displayName!!,
+                    uuid = it.identifiers.playerUUID!!
+                ),
+                EntityPositionComponent(UNIVERSAL_SPAWN, UNIVERSAL_ROT), // TODO: get proper spawn logic
+                EntityChunkLoadingComponent(radius = 4) // TODO: move this to chunk loading?
+            )
+        )
+        mapContext.onPlayerConnected(it.identifiers, eid)
+    }
+
+    private fun handleDisconnectedAction(it: PlayerDisconnectedAction) {
+        val eid = mapContext.findPlayerByUUID(it.identifiers.playerUUID!!)
+        mapContext.onPlayerDisconnected(it.identifiers)
+        if (eid != null) {
+            db.entities.destroy(eid)
+        }
     }
 }
