ac41c72e397d19018f07ed49488447a3d7b1f465
johnbanq <johnbanq@gmail.com>
2022-09-23 19:20:50 +0100

    feat(comm): initial command & chat support

--- server/src/main/kotlin/com/github/bedrockecs/vanilla/game/VanillaGameConfiguration.kt
+++ server/src/main/kotlin/com/github/bedrockecs/vanilla/game/VanillaGameConfiguration.kt
@@ -2,6 +2,7 @@ package com.github.bedrockecs.vanilla.game
 
 import com.github.bedrockecs.server.game.ext.GameConfiguration
 import com.github.bedrockecs.vanilla.data.VanillaBlockConfigurator
+import com.github.bedrockecs.vanilla.game.command.system.TestCommandSystem
 import com.github.bedrockecs.vanilla.game.pinteract.system.PlayerInteractionSystem
 import com.github.bedrockecs.vanilla.game.player.system.PlayerConnectDisconnectSystem
 import org.springframework.context.annotation.ComponentScan
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan
 @GameConfiguration
 @ComponentScan(
     basePackageClasses = [
+        TestCommandSystem::class,
         PlayerInteractionSystem::class,
         PlayerConnectDisconnectSystem::class,
         VanillaBlockConfigurator::class
