d8847502d88d22881314c16624a64aa9f84c51ab
johnbanq <johnbanq@gmail.com>
2022-09-24 16:55:24 +0100

    feat(plugin): initial plugin support

--- server/src/main/kotlin/com/github/bedrockecs/server/BedrockECS.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/BedrockECS.kt
@@ -5,13 +5,18 @@ import com.github.bedrockecs.server.filesystem.ServerFileSystem
 import com.github.bedrockecs.server.filesystem.zimpl.ServerFileSystemImpl
 import com.github.bedrockecs.server.filesystem.zimpl.initializeFileSystem
 import com.github.bedrockecs.server.game.zimpl.GameCoreConfiguration
+import com.github.bedrockecs.server.plugin.zimpl.loadPlugins
+import com.github.bedrockecs.server.plugin.zimpl.parseCLIArgs
+import com.github.bedrockecs.server.plugin.zimpl.scanFolder
 import com.github.bedrockecs.server.storegen.zimpl.StoreGenCoreConfiguration
 import com.github.bedrockecs.server.threading.zimpl.ThreadingCoreConfiguration
 import com.github.bedrockecs.server.zimpl.ChassisConfiguration
 import com.github.bedrockecs.vanilla.VanillaCoreConfiguration
+import org.springframework.boot.DefaultApplicationArguments
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.reactive.context.AnnotationConfigReactiveWebServerApplicationContext
 import org.springframework.web.reactive.config.EnableWebFlux
 import java.nio.file.Path
 import kotlin.io.path.absolute
@@ -40,6 +45,8 @@ class BedrockECS {
             intrinsicPlugins: Collection<Class<*>> = DEFAULT_INTRINSIC_CONFIGS,
             filesystem: ServerFileSystem? = null
         ) {
+            val parsedArgs = DefaultApplicationArguments(*args)
+
             val finalFileSystem = if (filesystem != null) {
                 filesystem
             } else {
@@ -48,10 +55,17 @@ class BedrockECS {
                 impl
             }
 
+            val foundPlugins = scanFolder(finalFileSystem.pluginDir) + parseCLIArgs(parsedArgs)
+            val loaded = loadPlugins(foundPlugins)
+
             runApplication<BedrockECS>(*args) {
                 sources = intrinsicPlugins.map { it.canonicalName }.toSet()
                 addInitializers({ ctx ->
                     ctx.beanFactory.registerSingleton("serverFileSystem", finalFileSystem)
+                    ctx as AnnotationConfigReactiveWebServerApplicationContext
+                    if (loaded.configurations.isNotEmpty()) {
+                        ctx.register(*loaded.configurations.toTypedArray())
+                    }
                 })
             }
         }
