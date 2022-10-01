6536824a4ebbb9abb5cfae8c0f960f9d751f4e22
johnbanq <johnbanq@gmail.com>
2022-10-01 22:54:38 +0100

    feat(datacompiler): add item analysis

--- datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/analyze/Analyze.kt
+++ datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/analyze/Analyze.kt
@@ -1,12 +1,24 @@
 package com.github.bedrockecs.datacompiler.analyze
 
 import com.github.bedrockecs.datacompiler.parse.ParseResult
+import com.github.bedrockecs.datacompiler.parse.id
+import com.github.bedrockecs.datacompiler.parse.persisentName
 import org.jetbrains.kotlinx.dataframe.DataFrame
+import org.jetbrains.kotlinx.dataframe.api.map
+import org.jetbrains.kotlinx.dataframe.api.toDataFrame
 
 fun analyze(parsed: ParseResult): AnalysisResult {
+    val itemDefs = analyzeItems(parsed)
+
     return AnalysisResult(
-        DataFrame.emptyOf(),
+        itemDefs,
         DataFrame.emptyOf(),
         DataFrame.emptyOf()
     )
+}
+
+fun analyzeItems(parsed: ParseResult): DataFrame<ItemTypeDefinition> {
+    return parsed.itemIds
+        .map { ItemTypeDefinition(persistentName = it.persisentName, itemId = it.id) }
+        .toDataFrame()
 }
