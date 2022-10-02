5cebc73b4b5684301df6552b8e2f95a73040437d
johnbanq <johnbanq@gmail.com>
2022-10-02 15:02:15 +0100

    feat(datacompiler): items codegen

--- datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/codegen/CodeGen.kt
+++ datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/codegen/CodeGen.kt
@@ -2,7 +2,20 @@ package com.github.bedrockecs.datacompiler.codegen
 
 import com.github.bedrockecs.datacompiler.analyze.AnalysisResult
 import com.squareup.kotlinpoet.FileSpec
+import org.jetbrains.kotlinx.dataframe.api.map
 
 fun codeGen(analyzed: AnalysisResult): List<FileSpec> {
-    return emptyList()
+    val ret = mutableListOf<FileSpec>()
+
+    run {
+        val itemTuples = analyzed.itemDefs.map { codegenItemType(it) }.toList()
+        val itemDefs = itemTuples.map { it.first }
+        val itemTypeNames = itemTuples.map { it.second }
+        val itemTypes = codegenItemTypes(itemTypeNames)
+
+        ret.addAll(itemDefs)
+        ret.add(itemTypes)
+    }
+
+    return ret
 }
--- datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/codegen/CodeGenConstants.kt
+++ datacompiler/src/main/kotlin/com/github/bedrockecs/datacompiler/codegen/CodeGenConstants.kt
@@ -1,5 +1,11 @@
 package com.github.bedrockecs.datacompiler.codegen
 
+import com.squareup.kotlinpoet.ClassName
+
 const val BASE_PACKAGE = "com.github.bedrockecs.vanilla.data"
 const val BLOCKS_BASE_PACKAGE = "$BASE_PACKAGE.blocks"
 const val ITEMS_BASE_PACKAGE = "$BASE_PACKAGE.items"
+
+val VANILLA_ITEM_TYPE = ClassName(ITEMS_BASE_PACKAGE, "VanillaItemType")
+val VANILLA_ITEM_TYPE_COMPANION = ClassName(ITEMS_BASE_PACKAGE, "VanillaItemType", "Companion")
+val ITEM_ID = ClassName(ITEMS_BASE_PACKAGE, "ItemID")
