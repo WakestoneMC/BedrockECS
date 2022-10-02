446c0cdfb557b7ca7e32850bd5b2807505388f84
johnbanq <johnbanq@gmail.com>
2022-10-02 14:35:52 +0100

    refactor(game): specify ITEM_TYPE in ItemTypeComponent's companion

--- server/src/main/kotlin/com/github/bedrockecs/server/game/db/invitem/data/ItemTypeComponent.kt
+++ server/src/main/kotlin/com/github/bedrockecs/server/game/db/invitem/data/ItemTypeComponent.kt
@@ -6,12 +6,22 @@ import com.github.bedrockecs.server.game.db.common.Component
  * represents the type of the item
  */
 interface ItemTypeComponent : ItemComponent {
-    companion object : Component.Companion {
-        override val TYPE: String = "becs:item_type"
+    /**
+     * spec for its companion object, with TYPE filled out for implementor
+     */
+    interface Companion : Component.Companion {
+        companion object {
+            val TYPE = "becs:item_type"
+        }
+
+        override val TYPE: String
+            get() = ItemTypeComponent.Companion.TYPE
+
+        val ITEM_TYPE: String
     }
 
     override val type: String
-        get() = TYPE
+        get() = Companion.TYPE
 
     val itemType: String
 }
--- server/src/main/kotlin/com/github/bedrockecs/vanilla/data/items/VanillaItemType.kt
+++ server/src/main/kotlin/com/github/bedrockecs/vanilla/data/items/VanillaItemType.kt
@@ -3,7 +3,7 @@ package com.github.bedrockecs.vanilla.data.items
 import com.github.bedrockecs.server.game.db.invitem.data.ItemTypeComponent
 
 interface VanillaItemType : ItemTypeComponent {
-    interface Companion {
+    interface Companion : ItemTypeComponent.Companion {
         val itemID: ItemID
     }
 }
